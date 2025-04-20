d64defa1ab69ba4707fffe4dfc3adfd70def0aa0
Shivam Prajapati <mrshivam073@gmail.com>
2025-04-20 08:55:42 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,108 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+// signUp & signIn starts
+
+// const signUpButton=document.getElementById('signUpButton');
+// const signInButton=document.getElementById('signInButton');
+// const signInForm=document.getElementById('signIn');
+// const signUpForm=document.getElementById('signUp');
+
+
+// signUpButton.addEventListener('click',function(){
+//     signInForm.style.display="none";
+//     signUpForm.style.display="block";
+// });
+
+// signInButton.addEventListener('click',function(){
+//     signInForm.style.display="block";
+//     signInForm.style.display="none";
+// });
+
+
+
+
+// signUp & signIn ends
+
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () =>{
+    themeToggler.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    themeToggler.classList.remove('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+
+    btn.onclick = () =>{
+        let color = btn.style.background;
+        document.querySelector(':root').style.setProperty('--main-color',color);
+    }
+
+});
+
+var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction:false,
+    }
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop:true,
+    spaceBetween: 10,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay:{
+        delay: 5000,
+        disableOnInteraction:false,
+    }
+});
+
+
